a78fc2ce403bb14f307bfd9fa4ddd8d7c9c5a9d4
rmliu <2218207429@qq.com>
2021-01-06 19:22:41 +0800

    add design patterns code.

--- DesignPatterns/src/main/java/org/copydays/rmliu/JUnit/Strategy/ArraySortableTests.java
+++ DesignPatterns/src/main/java/org/copydays/rmliu/JUnit/Strategy/ArraySortableTests.java
@@ -1,15 +1,15 @@
-package org.copydays.rmliu.JUnit.Strategy;
-
-import org.copydays.rmliu.Adapter.ListSortable;
-
-import java.lang.reflect.Array;
-import java.util.List;
-
-public class ArraySortableTests extends SortableTests {
-
-    @Override
-    protected Sortable<Integer> createSortable(Integer[] data) {
-        List<Integer> list = Array.asList(data);
-        return new ListSortable<Integer>(list);
-    }
-}
+//package org.copydays.rmliu.JUnit.Strategy;
+//
+//import org.copydays.rmliu.Adapter.ListSortable;
+//
+//import java.lang.reflect.Array;
+//import java.util.List;
+//
+//public class ArraySortableTests extends SortableTests {
+//
+//    @Override
+//    protected Sortable<Integer> createSortable(Integer[] data) {
+//        List<Integer> list = Array.asList(data);
+//        return new ListSortable<Integer>(list);
+//    }
+//}
