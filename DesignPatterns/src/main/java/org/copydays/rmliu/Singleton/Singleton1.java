fd7f535bad78a50b620b9a72f402eb4fed51c292
rmliu <2218207429@qq.com>
2021-01-06 12:11:24 +0800

    add singleton patterns code.

---
+++ DesignPatterns/src/main/java/org/copydays/rmliu/Singleton/Singleton1.java
@@ -0,0 +1,14 @@
+package org.copydays.rmliu.Singleton;
+
+public class Singleton1 {
+    private Singleton1() {
+
+    }
+
+    private static Singleton1 instance = new Singleton1();
+
+    public static Singleton1 getInstance() {
+        return instance;
+    }
+
+}
---
+++ DesignPatterns/src/main/java/org/copydays/rmliu/Singleton/Singleton2.java
@@ -0,0 +1,16 @@
+package org.copydays.rmliu.Singleton;
+
+public class Singleton2 {
+    private Singleton2() {
+
+    }
+
+    private static Singleton2 instance = null;
+
+    public static synchronized Singleton2 getInstance() {
+        if (instance == null) {
+            instance = new Singleton2();
+        }
+        return instance;
+    }
+}
